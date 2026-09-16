00c2aae643d3c29cf2ceeea287dabe87e410b265
zhydaa <zhyyamerta24@gmail.com>
2026-09-15 23:49:28 -0600

    tugas

---
+++ Pertemuan 2/variabel.py
@@ -0,0 +1,15 @@
+# =================================
+# PROGRAM PENGENALAN VARIABEL PY
+# =================================
+
+# Membuat variabel
+nama = "Mutiara Meyzhida Yulianti"
+umur = 18
+tinggi = 160
+sudah_menikah = False 
+
+# Menampilkan nilai dan tipe variabel
+print("Nama          :", nama,          "| Tipe :", type(nama))
+print("Umur          :", umur, "tahun", "| Tipe :", type(umur))
+print("Tinggi        :", tinggi, "cm",  "| Tipe :", type(tinggi))
+print("Sudah Menikah :", sudah_menikah, "| Tipe :", type(sudah_menikah))
